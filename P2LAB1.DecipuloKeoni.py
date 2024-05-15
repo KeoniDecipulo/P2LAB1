11b90f01c720f1ad7fc0527cccc25df5ff2bbb45
KeoniDecipulo <166351717+KeoniDecipulo@users.noreply.github.com>
2024-05-15 09:09:34 -0400

    Add files via upload

---
+++ P2LAB1.DecipuloKeoni.py
@@ -0,0 +1,18 @@
+# Keoni Decipulo
+# 4/14/2024
+# Variables and Expressions
+# P2LAB1
+import math
+radius=float(input("What is the radius of the circle?"))
+print()
+diameter=2*radius
+print(f' The diameter of the circle is {diameter:.1f}')
+print()
+circumference=2*math.pi*radius
+print(f'The circumference of the circle is {circumference:.2f}')
+print()
+area=math.pi*radius**2
+print(f'The area of the circle is {area:.3f}')
+
+
+      
